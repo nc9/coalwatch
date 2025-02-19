b27440988eb87ff49cded00688dc6d0e46a33087
Nik Cubrilovic <git@nikcub.me>
2025-02-19 17:45:20 +1100

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,19 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <title>Coal Watch Australia</title>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link rel="icon" type="image/png" />
+                <link rel="manifest" href="/site.webmanifest"></link>
             </head>
             <body className={`${inter.className} ${bebas.variable}`}>
                 {children}
